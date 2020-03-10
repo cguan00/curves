1d31d07da811c8fa0c495947c92652b7b795d391
Christy Guan <cguan00@stuy.edu>
2020-03-09 21:35:29 -0400

    back to old img

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from matrix import *
 import math
 
 screen = new_screen()
-color = [ 255, 0, 0 ]
+color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
 
@@ -19,4 +19,4 @@ transform = new_matrix()
 # print
 # print_matrix( make_rotZ(math.pi/4) )
 
-parse_file( 'script1', edges, transform, screen, color )
+parse_file( 'script', edges, transform, screen, color )
